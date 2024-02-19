bf1c6010dc76f3b83f0da9d0e49ce45d961d5dd8
baqari1991 <137100801+baqari1991@users.noreply.github.com>
2024-02-19 18:14:04 +0400

    -გაკეთებულია base.html. -დამატებულია urls -შექმნილია ფუნქციები -მთლიანად ინტეგრირებულია html და css django-ში -პროექტი მზად არის ფუნქციების ჩაშენებისთვის

--- hotel_project/hotelapp/urls.py
+++ hotel_project/hotelapp/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('amenities', views.amenities, name='amenities'),
+    path('contact', views.contact, name='contact'),
+    path('booking', views.booking, name='booking'),
+    path('login', views.login, name='login'),
+    path('room', views.room, name='room'),
 
 ]
--- hotel_project/hotelapp/views.py
+++ hotel_project/hotelapp/views.py
@@ -8,5 +8,25 @@ def index(request):
     return render(request, 'index.html')
 
 
+def amenities(request):
+    return render(request, 'amenities.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def booking(request):
+    return render(request, 'booking.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def room(request):
+    return render(request, 'room.html')
+
+
 def about(request):
     return render(request, 'about.html')
